b9ecfc929add5ab0cb8b9d3f069fef1b7cb624ac
= <coded_raf@yahoo.com>
2020-12-28 15:33:57 +0000

    Convert LiveData to StateFlow

--- app/src/main/java/com/foreverrafs/superdiary/framework/presentation/common/BaseViewModel.kt
+++ app/src/main/java/com/foreverrafs/superdiary/framework/presentation/common/BaseViewModel.kt
@@ -1,13 +1,13 @@
 package com.foreverrafs.superdiary.framework.presentation.common
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 abstract class BaseViewModel<T> : ViewModel() {
     private val _viewState: MutableStateFlow<T?> = MutableStateFlow(null)
 
-    val viewState = _viewState.asLiveData()
+    val viewState = _viewState.asStateFlow()
 
     fun setViewState(state: T) {
         _viewState.value = state
