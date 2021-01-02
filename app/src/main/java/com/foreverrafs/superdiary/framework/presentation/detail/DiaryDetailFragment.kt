9b5fb152d547a53c088047d4ce3e7d4599d2cb98
= <coded_raf@yahoo.com>
2021-01-02 20:58:42 +0000

    Add ability to scroll through diaries from detail page

--- app/src/main/java/com/foreverrafs/superdiary/framework/presentation/detail/DiaryDetailFragment.kt
+++ app/src/main/java/com/foreverrafs/superdiary/framework/presentation/detail/DiaryDetailFragment.kt
@@ -1,27 +1,34 @@
 package com.foreverrafs.superdiary.framework.presentation.detail
 
 import android.os.Bundle
-import android.text.InputType
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
-import com.foreverrafs.superdiary.R
+import androidx.viewpager2.widget.ViewPager2
+import com.foreverrafs.superdiary.business.model.Diary
 import com.foreverrafs.superdiary.databinding.FragmentDiaryDetailBinding
 import com.foreverrafs.superdiary.framework.presentation.common.BaseFragment
-import com.foreverrafs.superdiary.framework.presentation.util.invisible
-import java.time.LocalDate
+import com.foreverrafs.superdiary.framework.presentation.diarylist.DiaryListViewModel
+import com.foreverrafs.superdiary.framework.presentation.diarylist.state.DiaryListState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import java.time.format.DateTimeFormatter
 
-
+@AndroidEntryPoint
 class DiaryDetailFragment : BaseFragment<FragmentDiaryDetailBinding>() {
+    private val diaryListViewModel: DiaryListViewModel by viewModels()
+    private lateinit var diaryPagerAdapter: DiaryDetailPagerAdapter
+
     private val incomingArgs by navArgs<DiaryDetailFragmentArgs>()
     private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy")
     private val incomingDiary by lazy {
         incomingArgs.diary
     }
+    private lateinit var diaryList: List<Diary>
 
     override fun inflateBinding(
         inflater: LayoutInflater,
@@ -30,38 +37,56 @@ class DiaryDetailFragment : BaseFragment<FragmentDiaryDetailBinding>() {
         return FragmentDiaryDetailBinding.inflate(inflater, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
-        processIncomingDiary()
+
+        next.setOnClickListener {
+            diaryEntryPager.currentItem =
+                (diaryEntryPager.currentItem + 1).coerceAtMost(diaryList.size - 1)
+        }
+
+        previous.setOnClickListener {
+            diaryEntryPager.currentItem = (diaryEntryPager.currentItem - 1).coerceAtLeast(0)
+        }
+
+        lifecycleScope.launchWhenStarted {
+            diaryListViewModel.viewState.collect {
+                when (it) {
+                    is DiaryListState.DiaryList -> {
+                        diaryList = diaryListViewModel.allDiaries.reversed()
+                        diaryPagerAdapter = DiaryDetailPagerAdapter(diaryList)
+                        diaryEntryPager.adapter = diaryPagerAdapter
+                        processIncomingDiary()
+                    }
+
+                    else -> {
+
+                    }
+                }
+            }
+        }
+
+        diaryEntryPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                val diary = diaryList[position]
+                val date = diary.date.format(formatter)
+                tvDairyEntryDate.text = date
+
+                binding.next.isVisible = position < diaryList.size - 1
+                binding.previous.isVisible = position > 0
+            }
+        })
     }
 
-    private fun processIncomingDiary() {
+
+    private fun processIncomingDiary() = lifecycleScope.launchWhenStarted {
         with(binding) {
             val date = incomingDiary.date.format(formatter)
-
             tvDairyEntryDate.text = date
-            textDiaryEntry.setText(incomingDiary.message)
 
-            if (incomingDiary.date.toLocalDate() != LocalDate.now()) {
-                btnEditDiary.invisible()
-
-                // TODO: 29/12/20 Disable user input  
-
-                //center the delete button if we are unable to edit
-                val params =
-                    CoordinatorLayout.LayoutParams(
-                        CoordinatorLayout.LayoutParams.WRAP_CONTENT,
-                        CoordinatorLayout.LayoutParams.WRAP_CONTENT
-                    )
-
-                params.gravity = Gravity.BOTTOM or Gravity.CENTER
-                params.bottomMargin = resources.getDimension(R.dimen.space).toInt()
-
-                btnDeleteDiary.layoutParams = params
-            } else {
-                binding.textDiaryEntry.inputType =
-                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_MULTI_LINE;
-            }
+            val position = diaryList.indexOf(incomingDiary)
+            binding.diaryEntryPager.currentItem = position
         }
     }
 
