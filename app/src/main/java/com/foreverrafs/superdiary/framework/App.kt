f2d1be00a467b1d128324100b2e93925cc4e6d52
= <coded_raf@yahoo.com>
2021-02-18 22:58:23 +0000

    Rename App class

--- app/src/main/java/com/foreverrafs/superdiary/framework/App.kt
+++
@@ -1,7 +0,0 @@
-package com.foreverrafs.superdiary.framework
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application()
---
+++ app/src/main/java/com/foreverrafs/superdiary/framework/SuperDiaryApp.kt
@@ -0,0 +1,7 @@
+package com.foreverrafs.superdiary.framework
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SuperDiaryApp : Application()
