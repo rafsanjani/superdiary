b7ff7e0fe549fb473463436041d7c581216f4ec6
Rafsanjani Aziz <coded_raf@yahoo.com>
2022-01-02 02:43:18 +0000

    Fix test case for GetAllDiariesUseCaseTest.kt

--- app/src/test/java/com/foreverrafs/superdiary/business/usecase/diarylist/GetAllDiariesUseCaseTest.kt
+++ app/src/test/java/com/foreverrafs/superdiary/business/usecase/diarylist/GetAllDiariesUseCaseTest.kt
@@ -1,7 +1,12 @@
 package com.foreverrafs.superdiary.business.usecase.diarylist
 
+import app.cash.turbine.test
+import com.foreverrafs.superdiary.business.Result
 import com.foreverrafs.superdiary.business.data.DependenciesInjector
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 
 class GetAllDiariesUseCaseTest {
     private lateinit var getAllDiaries: GetAllDiariesUseCase
@@ -13,10 +18,12 @@ class GetAllDiariesUseCaseTest {
         getAllDiaries = GetAllDiariesUseCase(repository)
     }
 
-//    @Test
-//    fun `fetch all diaries confirm fetched`() = runBlocking {
-//        getAllDiaries().collect {
-//            assertThat(it).isNotEmpty()
-//        }
-//    }
+    @Test
+    fun `fetch all diaries confirm fetched`() = runBlocking {
+        getAllDiaries().test {
+            val result = expectMostRecentItem()
+            assertThat(result).isInstanceOf(Result.Success::class.java)
+            assertThat((result as Result.Success).data).isNotEmpty()
+        }
+    }
 }
