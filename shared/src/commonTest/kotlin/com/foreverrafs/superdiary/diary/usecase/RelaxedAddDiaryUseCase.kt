6505d0be6902caaa58732e5b49e6600ba80d5c18
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-03-08 08:11:52 +0000

    Add doc for fake use case used for unit tests

--- shared/src/commonTest/kotlin/com/foreverrafs/superdiary/diary/usecase/RelaxedAddDiaryUseCase.kt
+++ shared/src/commonTest/kotlin/com/foreverrafs/superdiary/diary/usecase/RelaxedAddDiaryUseCase.kt
@@ -5,9 +5,10 @@ import com.foreverrafs.superdiary.diary.datasource.DataSource
 import com.foreverrafs.superdiary.diary.model.Diary
 
 /**
- * Adds diaries to our local datasource without enforcing any data integrity checks
+ * Adds diaries to our local datasource without enforcing any data integrity checks.
+ * @see [AddDiaryUseCase] for the original version of this
  */
-class RelaxedAddDiaryUseCase(private val dataSource: DataSource) {
+internal class RelaxedAddDiaryUseCase(private val dataSource: DataSource) {
     suspend operator fun invoke(diary: Diary): Result {
         return try {
             dataSource.add(diary)
