4c30f8a82221cda17a6008cd8027a3d24315d1a0
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-05-08 06:38:45 +0100

    Convert Result to a sealed interface and remove unused Loading object

--- shared/src/commonMain/kotlin/com/foreverrafs/superdiary/diary/Result.kt
+++ shared/src/commonMain/kotlin/com/foreverrafs/superdiary/diary/Result.kt
@@ -2,8 +2,7 @@ package com.foreverrafs.superdiary.diary
 
 import com.foreverrafs.superdiary.diary.model.Diary
 
-sealed class Result {
-    data class Success(val data: List<Diary>) : Result()
-    data class Failure(val error: Throwable) : Result()
-    object Loading : Result()
+sealed interface Result {
+    data class Success(val data: List<Diary>) : Result
+    data class Failure(val error: Throwable) : Result
 }
