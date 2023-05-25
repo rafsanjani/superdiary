7605c9fa8fa9e9885060e6c9994b5c5c22b327d0
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-05-25 16:46:54 +0100

    Fix diary grouping tests

--- shared/src/commonTest/kotlin/com/foreverrafs/superdiary/diary/usecase/DiaryGroupingTest.kt
+++ shared/src/commonTest/kotlin/com/foreverrafs/superdiary/diary/usecase/DiaryGroupingTest.kt
@@ -4,37 +4,38 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.foreverrafs.superdiary.diary.model.Diary
 import com.foreverrafs.superdiary.diary.utils.groupByDate
-import kotlin.random.Random
-import kotlin.test.Test
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
-import kotlinx.datetime.TimeZone
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.plus
+import kotlin.random.Random
+import kotlin.test.Test
 
 class DiaryGroupingTest {
+    private val clock = object : Clock {
+        override fun now(): Instant = Instant.parse(isoString = "2023-05-03T01:01:01.049Z")
+    }
 
     @Test
     fun `test diary groupings by day`() {
+
         val diaries = createDiaries(
             durationSpacing = DateTimeUnit.DAY,
-            startDate = Instant.parse(isoString = "2023-05-01T01:01:01.049Z"),
+            startDate = LocalDate.parse(isoString = "2023-05-01"),
             count = 3
         )
-        val groups = diaries.groupByDate()
+
+        val groups = diaries.groupByDate(clock)
 
         assertThat(groups.size).isEqualTo(3)
     }
 
     @Test
     fun `test diary groupings by week`() {
-        val clock = object : Clock {
-            override fun now(): Instant = Instant.parse(isoString = "2023-01-28T01:01:01.049Z")
-        }
-
         val diaries = createDiaries(
             durationSpacing = DateTimeUnit.WEEK,
-            startDate = Instant.parse(isoString = "2023-01-01T01:01:01.049Z"),
+            startDate = LocalDate.parse(isoString = "2023-05-01"),
             count = 3
         )
         val groups = diaries.groupByDate(clock)
@@ -44,13 +45,10 @@ class DiaryGroupingTest {
 
     @Test
     fun `test diary groupings by months`() {
-        val clock = object : Clock {
-            override fun now(): Instant = Instant.parse(isoString = "2023-04-01T01:01:01.049Z")
-        }
 
         val diaries = createDiaries(
             durationSpacing = DateTimeUnit.MONTH,
-            startDate = Instant.parse(isoString = "2023-01-01T01:01:01.049Z"),
+            startDate = LocalDate.parse(isoString = "2023-01-01"),
             count = 3
         )
         val groups = diaries.groupByDate(clock)
@@ -60,13 +58,9 @@ class DiaryGroupingTest {
 
     @Test
     fun `test diary groupings by months and weeks`() {
-        val clock = object : Clock {
-            override fun now(): Instant = Instant.parse(isoString = "2023-05-08T01:01:01.049Z")
-        }
-
         val diaries = createDiaries(
             durationSpacing = DateTimeUnit.WEEK,
-            startDate = Instant.parse(isoString = "2023-04-08T01:01:01.049Z"),
+            startDate = LocalDate.parse(isoString = "2023-04-08"),
             count = 4
         )
         val groups = diaries.groupByDate(clock)
@@ -75,8 +69,8 @@ class DiaryGroupingTest {
     }
 
     private fun createDiaries(
-        durationSpacing: DateTimeUnit,
-        startDate: Instant,
+        durationSpacing: DateTimeUnit.DateBased,
+        startDate: LocalDate,
         count: Int,
     ): List<Diary> {
         return (0 until count).map {
@@ -84,9 +78,8 @@ class DiaryGroupingTest {
                 id = Random.nextLong(),
                 entry = "Diary Entry #$it",
                 date = startDate.plus(
-                    it.toLong(),
-                    durationSpacing,
-                    TimeZone.currentSystemDefault()
+                    value = it.toLong(),
+                    unit = durationSpacing,
                 ).toString(),
             )
         }
