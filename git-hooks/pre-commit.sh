cf2c1326d0a790bdd2e9578a40d68f94c0e40213
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-06-01 21:39:23 +0100

    Update pre-commit hook

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 
-######## KTLINT-GRADLE HOOK START ########
+######## DETEKT-GRADLE HOOK START ########
 
 CHANGED_FILES="$(git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~ /\.kts|\.kt/ { print $2}')"
 
 if [ -z "$CHANGED_FILES" ]; then
-    echo "No Kotlin staged files."
-    exit 0
-fi;
+  echo "No Kotlin staged files."
+  exit 0
+fi
 
-echo "Running detekt over these files:"
+echo "Running detekt"
 echo "$CHANGED_FILES"
 
-./gradlew --quiet ktlintFormat -PinternalKtlintGitFilter="$CHANGED_FILES"
-detekt --input "$CHANGED_FILES" 2>&1
+/gradlew androidApp:detekt
 
-echo "Completed ktlint run."
+echo "Completed detekt run."
 
 echo "$CHANGED_FILES" | while read -r file; do
-    if [ -f $file ]; then
-        git add $file
-    fi
+  if [ -f "$file" ]; then
+    git add "$file"
+  fi
 done
 
-echo "Completed ktlint hook."
 ######## DETEKT-GRADLE HOOK END ########
