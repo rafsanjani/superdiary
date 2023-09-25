86f42c5a9b38a25602c601c42a00d7721132f641
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-09-25 23:21:15 +0100

    Consume padding values from Scaffold

--- androidApp/src/main/java/com/foreverrafs/superdiary/android/MainActivity.kt
+++ androidApp/src/main/java/com/foreverrafs/superdiary/android/MainActivity.kt
@@ -3,17 +3,9 @@ package com.foreverrafs.superdiary.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.foreverrafs.superdiary.android.di.ApplicationComponent
 import com.foreverrafs.superdiary.ui.App
 
 class MainActivity : ComponentActivity() {
-    private val appComponent by lazy {
-        ApplicationComponent.from(this)
-    }
-
-    private val appScreens by lazy {
-        appComponent.appScreens
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
