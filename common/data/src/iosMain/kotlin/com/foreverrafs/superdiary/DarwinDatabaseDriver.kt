670765435941a85367658581104be9a0782412dd
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-09-26 21:28:34 +0100

    Fix ios build issues

--- common/data/src/iosMain/kotlin/com/foreverrafs/superdiary/DarwinDatabaseDriver.kt
+++ common/data/src/iosMain/kotlin/com/foreverrafs/superdiary/DarwinDatabaseDriver.kt
@@ -1,5 +1,6 @@
 package com.foreverrafs.superdiary
 
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.foreverrafs.superdiary.diary.DatabaseDriver
@@ -7,6 +8,6 @@ import db.KmpSuperDiaryDB
 
 class DarwinDatabaseDriver : DatabaseDriver {
     override fun createDriver(): SqlDriver {
-        return NativeSqliteDriver(KmpSuperDiaryDB.Schema, "diary.db")
+        return NativeSqliteDriver(KmpSuperDiaryDB.Schema.synchronous(), "diary.db")
     }
 }
