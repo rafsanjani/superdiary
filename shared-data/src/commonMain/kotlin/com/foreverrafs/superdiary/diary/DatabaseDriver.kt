dde924897e830191f6377b20fb292ab8be0b9ec2
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-10-22 21:24:46 +0100

    Lint code a bit

--- shared-data/src/commonMain/kotlin/com/foreverrafs/superdiary/diary/DatabaseDriver.kt
+++ shared-data/src/commonMain/kotlin/com/foreverrafs/superdiary/diary/DatabaseDriver.kt
@@ -2,6 +2,6 @@ package com.foreverrafs.superdiary.diary
 
 import app.cash.sqldelight.db.SqlDriver
 
-interface DatabaseDriver {
+fun interface DatabaseDriver {
     fun createDriver(): SqlDriver
 }
