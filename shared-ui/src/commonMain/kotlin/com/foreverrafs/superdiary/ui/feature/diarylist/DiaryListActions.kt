841aedbaaeb2feffdfe8ebe83e8c87f512be74ba
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-10-23 20:43:11 +0100

    Extract DiaryListActions to separate file

---
+++ shared-ui/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/feature/diarylist/DiaryListActions.kt
@@ -0,0 +1,16 @@
+package com.foreverrafs.superdiary.ui.feature.diarylist
+
+import com.foreverrafs.superdiary.diary.model.Diary
+
+data class DiaryListActions(
+    val onAddEntry: () -> Unit,
+    val onDeleteDiaries: suspend (selectedIds: List<Diary>) -> Boolean,
+    val onToggleFavorite: suspend (diary: Diary) -> Boolean,
+    val onApplyFilters: (filters: DiaryFilters) -> Unit,
+    val onAddSelection: ((id: Long?) -> Unit) = { },
+    val onRemoveSelection: (id: Long?) -> Unit = {},
+    val onToggleSelection: (id: Long?) -> Unit = {},
+    val onCancelSelection: () -> Unit = {},
+) {
+    companion object
+}
--- shared-ui/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/feature/diarylist/DiaryListScreen.kt
+++ shared-ui/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/feature/diarylist/DiaryListScreen.kt
@@ -69,19 +69,6 @@ import kotlinx.coroutines.launch
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
-data class DiaryListActions(
-    val onAddEntry: () -> Unit,
-    val onDeleteDiaries: suspend (selectedIds: List<Diary>) -> Boolean,
-    val onToggleFavorite: suspend (diary: Diary) -> Boolean,
-    val onApplyFilters: (filters: DiaryFilters) -> Unit,
-    val onAddSelection: ((id: Long?) -> Unit) = { },
-    val onRemoveSelection: (id: Long?) -> Unit = {},
-    val onToggleSelection: (id: Long?) -> Unit = {},
-    val onCancelSelection: () -> Unit = {},
-) {
-    companion object
-}
-
 val DiaryListActions.Companion.Empty: DiaryListActions
     get() = DiaryListActions(
         onCancelSelection = {},
