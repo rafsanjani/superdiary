bc67bbb0ecf4fbf263863379bec5cf4cb5b58004
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-11-05 21:17:46 +0000

    Fix koin error

--- shared-data/src/iosMain/kotlin/com/foreverrafs/superdiary/diary/di/Modules.ios.kt
+++ shared-data/src/iosMain/kotlin/com/foreverrafs/superdiary/diary/di/Modules.ios.kt
@@ -1,8 +1,11 @@
 package com.foreverrafs.superdiary.diary.di
 
+import com.foreverrafs.superdiary.DarwinDatabaseDriver
+import com.foreverrafs.superdiary.diary.DatabaseDriver
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 actual fun platformModule(): Module = module {
-    factory { }
+    factoryOf<DatabaseDriver>(::DarwinDatabaseDriver)
 }
