64da654dfa7ce9d055dfbc0bc3f6c7d7658e3a91
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-11-07 18:00:09 +0000

    Rename main.ios.kt to ViewController.kt

---
+++ shared-ui/src/iosMain/kotlin/com/foreverrafs/superdiary/ui/ViewController.kt
@@ -0,0 +1,17 @@
+@file:Suppress("FunctionName", "Unused")
+
+package com.foreverrafs.superdiary.ui
+
+import androidx.compose.ui.uikit.OnFocusBehavior
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
+
+
+class ViewController {
+    fun mainViewController(): UIViewController = ComposeUIViewController(
+        configure = {
+            onFocusBehavior = OnFocusBehavior.DoNothing
+        },
+        content = { App() }
+    )
+}
