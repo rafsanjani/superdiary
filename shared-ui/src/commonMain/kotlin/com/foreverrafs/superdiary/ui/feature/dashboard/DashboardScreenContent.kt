5812ef7be96e1974e12867674b8a7824e15e6878
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-12-14 09:25:23 +0000

    Allow dismissing individual cards

--- shared-ui/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/feature/dashboard/DashboardScreenContent.kt
+++ shared-ui/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/feature/dashboard/DashboardScreenContent.kt
@@ -3,6 +3,7 @@ package com.foreverrafs.superdiary.ui.feature.dashboard
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -16,13 +17,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -31,6 +34,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,7 +45,9 @@ import androidx.compose.ui.unit.sp
 import com.foreverrafs.superdiary.diary.model.Diary
 import com.foreverrafs.superdiary.ui.feature.diarylist.screen.DiaryItem
 import com.foreverrafs.superdiary.ui.format
+import kotlin.random.Random
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DashboardScreenContent(
     state: DashboardScreenModel.DashboardScreenState,
@@ -49,83 +55,115 @@ fun DashboardScreenContent(
 ) {
     if (state !is DashboardScreenModel.DashboardScreenState.Content) return
 
+    val dashboardItems = remember {
+        mutableStateListOf(
+            DashboardSection(
+                content = {
+                    AtAGlance(
+                        modifier = Modifier
+                            .animateItemPlacement()
+                            .fillMaxWidth(),
+                        state = state,
+                    )
+                },
+            ),
+            DashboardSection(
+                content = { onDismiss ->
+                    WeeklySummaryCard(
+                        modifier = Modifier.animateItemPlacement()
+                            .fillMaxWidth()
+                            .heightIn(max = 200.dp, min = 150.dp),
+                        summary = state.weeklySummary,
+                        onDismiss = onDismiss,
+                    )
+                },
+            ),
+            DashboardSection(
+                content = {
+                    if (state.latestEntries.isNotEmpty()) {
+                        LatestEntries(
+                            modifier = Modifier.animateItemPlacement(),
+                            diaries = state.latestEntries,
+                            onSeeAll = onSeeAll,
+                        )
+                    }
+                },
+            ),
+        )
+    }
+
     Scaffold {
-        Column(
+        LazyColumn(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(items = dashboardItems, key = { it.id }) { content ->
+                content.content(this) {
+                    dashboardItems.remove(dashboardItems.firstOrNull { it.id == content.id })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun AtAGlance(
+    state: DashboardScreenModel.DashboardScreenState.Content,
+    modifier: Modifier = Modifier,
+) {
+    val animationState = remember { MutableTransitionState(false) }
+
+    val streaks by animateIntAsState(
+        targetValue = if (animationState.targetState) state.streak.count else 0,
+        animationSpec = tween(durationMillis = 1000),
+    )
+
+    val totalEntries by animateIntAsState(
+        targetValue = if (animationState.targetState) state.totalEntries.toInt() else 0,
+        animationSpec = tween(durationMillis = 1000),
+    )
+
+    LaunchedEffect(Unit) {
+        animationState.targetState = true
+    }
+
+    Column(modifier = modifier) {
+        Text(
+            text = "At a glance...",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(horizontal = 4.dp),
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             val dashboardCardModifier = Modifier
                 .weight(1f)
                 .aspectRatio(1f)
-
-            val itemSpacing = 8.dp
-
-            val animationState = remember { MutableTransitionState(false) }
-
-            val streaks by animateIntAsState(
-                targetValue = if (animationState.targetState) state.streak.count else 0,
-                animationSpec = tween(durationMillis = 1000),
+            GlanceCard(
+                modifier = dashboardCardModifier,
+                title = "Entries",
+                content = totalEntries.toString(),
             )
 
-            val totalEntries by animateIntAsState(
-                targetValue = if (animationState.targetState) state.totalEntries.toInt() else 0,
-                animationSpec = tween(durationMillis = 1000),
+            GlanceCard(
+                modifier = dashboardCardModifier,
+                title = "Streak 🔥",
+                content = "$streaks days",
+                caption = state.streak.dates.joinToString(" - ") { it?.format("MMM dd") ?: "" },
             )
 
-            LaunchedEffect(Unit) {
-                animationState.targetState = true
-            }
-
-            Text(
-                text = "At a glance...",
-                style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.padding(horizontal = 4.dp),
+            GlanceCard(
+                modifier = dashboardCardModifier,
+                title = "Best Streak",
+                content = "$streaks days",
+                caption = "Jun 20 - Jul 20",
             )
-
-            Spacer(modifier = Modifier.height(12.dp))
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(itemSpacing),
-            ) {
-                GlanceCard(
-                    modifier = dashboardCardModifier,
-                    title = "Entries",
-                    content = totalEntries.toString(),
-                )
-
-                GlanceCard(
-                    modifier = dashboardCardModifier,
-                    title = "Streak 🔥",
-                    content = "$streaks days",
-                    caption = state.streak.dates.joinToString(" - ") { it?.format("MMM dd") ?: "" },
-                )
-
-                GlanceCard(
-                    modifier = dashboardCardModifier,
-                    title = "Best Streak",
-                    content = "$streaks days",
-                    caption = "Jun 20 - Jul 20",
-                )
-            }
-
-            Spacer(modifier = Modifier.height(itemSpacing))
-
-            WeeklySummaryCard(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(max = 200.dp, min = 150.dp),
-                summary = state.weeklySummary,
-            )
-
-            Spacer(
-                modifier = Modifier
-                    .height(4.dp),
-            )
-
-            if (state.latestEntries.isNotEmpty()) {
-                LatestEntries(diaries = state.latestEntries, onSeeAll = onSeeAll)
-            }
         }
     }
 }
@@ -142,7 +180,9 @@ fun GlanceCard(
         shape = RoundedCornerShape(8.dp),
     ) {
         Column(
-            modifier = Modifier.fillMaxSize().padding(8.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
@@ -186,38 +226,42 @@ private fun LatestEntries(
                     onClick = onSeeAll,
                 )
                 .fillMaxWidth()
-                .padding(vertical = 8.dp),
+                .padding(bottom = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
         ) {
             Text(text = "Latest Entries", style = MaterialTheme.typography.headlineMedium)
 
             Spacer(modifier = Modifier.weight(1f))
-            Icon(
-                imageVector = Icons.Default.KeyboardArrowRight,
-                contentDescription = null,
-            )
+
+            Text("Show All", style = MaterialTheme.typography.labelSmall)
+//            Icon(
+//                imageVector = Icons.Default.KeyboardArrowRight,
+//                contentDescription = null,
+//            )
         }
 
-        LazyColumn(
+        Column(
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            items(diaries) {
+            for (diary in diaries) {
                 DiaryItem(
                     modifier = Modifier.clickable(onClick = onSeeAll),
                     inSelectionMode = false,
                     onToggleFavorite = {},
                     selected = false,
-                    diary = it,
+                    diary = diary,
                 )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun WeeklySummaryCard(
     modifier: Modifier = Modifier,
     summary: String?,
+    onDismiss: () -> Unit,
 ) {
     Card(
         modifier = modifier.verticalScroll(rememberScrollState()),
@@ -229,13 +273,24 @@ private fun WeeklySummaryCard(
                 .padding(8.dp),
             horizontalAlignment = Alignment.Start,
         ) {
-            Text(
-                text = "Weekly Summary",
-                modifier = Modifier.fillMaxWidth()
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(4.dp),
-                textAlign = TextAlign.Start,
-                style = MaterialTheme.typography.headlineMedium,
-            )
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = "Weekly Summary",
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.headlineMedium,
+                )
+
+                Icon(
+                    modifier = Modifier.clickable(onClick = onDismiss),
+                    imageVector = Icons.Default.Close,
+                    contentDescription = null,
+                )
+            }
 
             Text(
                 modifier = Modifier
@@ -259,3 +314,8 @@ private fun WeeklySummaryCard(
         }
     }
 }
+
+data class DashboardSection(
+    val content: @Composable LazyItemScope.(onDismiss: () -> Unit) -> Unit,
+    val id: Long = Random.nextLong(),
+)
