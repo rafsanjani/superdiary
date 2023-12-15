8a57ee65db1699fc6dbac8854ddcda36eaa88c62
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-12-15 15:28:57 +0000

    Add test for date formatting

---
+++ shared-ui/src/commonTest/kotlin/com/foreverrafs/superdiary/DateFormatterTest.kt
@@ -0,0 +1,17 @@
+package com.foreverrafs.superdiary
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.foreverrafs.superdiary.ui.format
+import kotlinx.datetime.LocalDate
+import kotlin.test.Test
+
+class DateFormatterTest {
+    @Test
+    fun `Verify date formatting works properly`() {
+        val date = LocalDate(2020, 10, 10)
+        val formattedDate = date.format("YYYY")
+        
+        assertThat(formattedDate).isEqualTo("2020")
+    }
+}
