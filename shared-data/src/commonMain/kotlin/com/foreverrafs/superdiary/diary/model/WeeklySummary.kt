2ce7df43871b7e649ff58cf535eba936d116f839
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-12-17 23:08:41 +0000

    Update dashboard screen VM tests

--- shared-data/src/commonMain/kotlin/com/foreverrafs/superdiary/diary/model/WeeklySummary.kt
+++ shared-data/src/commonMain/kotlin/com/foreverrafs/superdiary/diary/model/WeeklySummary.kt
@@ -1,8 +1,9 @@
 package com.foreverrafs.superdiary.diary.model
 
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
 data class WeeklySummary(
     val summary: String,
-    val date: Instant,
+    val date: Instant = Clock.System.now(),
 )
