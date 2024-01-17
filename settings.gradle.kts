ec09e3314a44306af3e74029432554cb3dd36508
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-01-17 07:07:24 +0000

    Upgrade versions plugin This includes the paparazzi workaround to get tests passing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("libs") {
-            from("io.github.rafsanjani:versions:2024.01.07")
+            from("io.github.rafsanjani:versions:2024.01.15")
         }
     }
 }
