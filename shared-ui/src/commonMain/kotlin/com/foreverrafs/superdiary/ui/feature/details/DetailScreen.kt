a596f1f6e7ea6068060c6802edd471ed986052e3
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-01-23 08:46:55 +0000

    Implement diary detail screen

--- shared-ui/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/feature/details/DetailScreen.kt
+++ shared-ui/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/feature/details/DetailScreen.kt
@@ -1,22 +1,18 @@
 package com.foreverrafs.superdiary.ui.feature.details
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import com.foreverrafs.superdiary.diary.model.Diary
+import com.foreverrafs.superdiary.ui.LocalScreenNavigator
 
 class DetailScreen(val diary: Diary) : Screen {
     @Composable
     override fun Content() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text("Diary Detail Screen")
-        }
+        val navigator = LocalScreenNavigator.current
+
+        DetailScreenContent(
+            onNavigateBack = navigator::pop,
+            diary = diary,
+        )
     }
 }
