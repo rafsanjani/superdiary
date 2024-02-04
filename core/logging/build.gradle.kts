3d32287fb46f7307af23952793fc57914c09908c
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-02-04 09:28:29 +0000

    Add new core modules

--- core/logging/build.gradle.kts
+++ core/logging/build.gradle.kts
@@ -4,35 +4,12 @@ import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING
 import java.io.IOException
 
 plugins {
-    alias(libs.plugins.ksp)
     alias(libs.plugins.android.library)
-    alias(libs.plugins.sqldelight)
     kotlin("multiplatform")
     id("kotlin-parcelize")
-    alias(libs.plugins.buildKonfig)
     alias(libs.plugins.sonar)
-    alias(libs.plugins.mockmp)
     alias(libs.plugins.testLogger)
     id("com.superdiary.kover")
-}
-
-buildkonfig {
-    packageName = "com.foreverrafs.superdiary.buildKonfig"
-
-    val openAiKey =
-        project.findProperty("openAiKey") ?: throw IOException("OpenAI API key not provided!")
-
-    // default config is required
-    defaultConfigs {
-        buildConfigField(STRING, "openAIKey", openAiKey.toString())
-    }
-}
-
-sqldelight {
-    databases.register("SuperDiaryDatabase") {
-        packageName.set("com.foreverrafs.superdiary.database")
-        deriveSchemaFromMigrations.set(true)
-    }
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -57,24 +34,13 @@ kotlin {
                 implementation(libs.touchlab.stately)
                 implementation(libs.koin.core)
                 implementation(libs.kotlin.inject.runtime)
-                implementation(libs.square.sqldelight.coroutinesExt)
                 implementation(libs.kotlin.coroutines.test)
-                implementation(libs.openAiKotlin)
                 implementation(libs.touchlab.kermit)
-                runtimeOnly(libs.ktor.client.cio)
             }
         }
         androidMain {
             dependencies {
-                implementation(libs.square.sqldelight.driver.android)
-                implementation(libs.square.sqldelight.coroutinesExt)
                 implementation(libs.koin.android)
-            }
-        }
-
-        val androidUnitTest by getting {
-            dependencies {
-                implementation(libs.square.sqldelight.driver.sqlite)
             }
         }
 
@@ -86,30 +52,19 @@ kotlin {
                 implementation(libs.kotlin.coroutines.test)
                 implementation(libs.turbine)
                 implementation(libs.assertk.common)
-                implementation(libs.mockmp.runtime)
             }
-            kotlin.srcDir("build/generated/ksp/jvm/jvmTest/kotlin")
         }
 
         iosMain {
             dependencies {
-                implementation(libs.square.sqldelight.driver.native)
                 implementation(libs.kotlin.coroutines.core)
-                implementation(libs.ktor.client.darwin)
-                implementation(libs.ktor.client.ios)
-            }
-        }
-
-        jvmMain {
-            dependencies {
-                implementation(libs.square.sqldelight.driver.sqlite)
             }
         }
     }
 }
 
 android {
-    namespace = "com.foreverrafs.data"
+    namespace = "com.foreverrafs.core.logging"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
@@ -120,9 +75,4 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
         sourceCompatibility = JavaVersion.VERSION_17
     }
-}
-
-mockmp {
-    usesHelper = true
-    installWorkaround()
 }
