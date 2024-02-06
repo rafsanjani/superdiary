8036cd9e507f162feff8e7455bc35be4872e91a7
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-02-06 07:06:12 +0000

    Add more test cases for swipe

--- swipe/src/commonTest/kotlin/me/saket/swipe/SwipeableActionStateTest.kt
+++ swipe/src/commonTest/kotlin/me/saket/swipe/SwipeableActionStateTest.kt
@@ -3,16 +3,19 @@ package me.saket.swipe
 import androidx.compose.runtime.MonotonicFrameClock
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import kotlin.math.roundToInt
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import kotlinx.coroutines.withContext
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class SwipeableActionStateTest {
     private lateinit var swipeableActionState: SwipeableActionState
     private val iconWidthPx = 200
@@ -31,7 +34,7 @@ class SwipeableActionStateTest {
 
     @Test
     fun `Resetting swipe should move offset back to zero`() = runTest {
-        withContext(TestClock) {
+        withContext(TestMonotonicFrameClock) {
             swipeableActionState.dragBy(-100f)
             swipeableActionState.resetDrag()
             assertThat(swipeableActionState.offset.value).isEqualTo(0.0f)
@@ -40,13 +43,39 @@ class SwipeableActionStateTest {
 
     @Test
     fun `Swiping on an item should reveal it's action box`() = runTest {
-        withContext(TestClock) {
+        withContext(TestMonotonicFrameClock) {
             swipeableActionState.dragBy(-100.0f)
             assertThat(swipeableActionState.offset.value).isEqualTo(-100.0f)
         }
     }
 
-    object TestClock : MonotonicFrameClock {
+    @Test
+    fun `Finalize drag should do nothing if item is at correct position`() = runTest {
+        withContext(TestMonotonicFrameClock) {
+            swipeableActionState.finalizeDrag()
+            assertThat(swipeableActionState.offset.value).isEqualTo(0f)
+        }
+    }
+
+    @Test
+    fun `Should drag to finish if item is dragged past threshold`() = runTest {
+        withContext(TestMonotonicFrameClock) {
+            swipeableActionState.dragBy(-(iconWidthPx / 2).toFloat())
+            swipeableActionState.finalizeDrag()
+            assertThat(swipeableActionState.offset.value.roundToInt()).isEqualTo(-iconWidthPx)
+        }
+    }
+
+    @Test
+    fun `Should reset drag when item is not dragged past threshold`() = runTest {
+        withContext(TestMonotonicFrameClock) {
+            swipeableActionState.dragBy(-(iconWidthPx / 3).toFloat())
+            swipeableActionState.finalizeDrag()
+            assertThat(swipeableActionState.offset.value.roundToInt()).isEqualTo(0)
+        }
+    }
+
+    object TestMonotonicFrameClock : MonotonicFrameClock {
         private var time = 0L
 
         override suspend fun <R> withFrameNanos(onFrame: (frameTimeNanos: Long) -> R): R {
