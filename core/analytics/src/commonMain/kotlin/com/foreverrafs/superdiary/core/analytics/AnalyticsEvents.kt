b85be42b4f5ac6d61ab79ea50b1ea0167753cf0a
Rafsanjani Aziz <9197459+rafsanjani@users.noreply.github.com>
2024-02-19 06:48:28 +0000

    Use ios-friendly name for analytics (#389)

--- core/analytics/src/commonMain/kotlin/com/foreverrafs/superdiary/core/analytics/AnalyticsEvents.kt
+++ core/analytics/src/commonMain/kotlin/com/foreverrafs/superdiary/core/analytics/AnalyticsEvents.kt
@@ -1,3 +1,8 @@
 package com.foreverrafs.superdiary.core.analytics
 
+import kotlin.experimental.ExperimentalObjCName
+import kotlin.native.ObjCName
+
+@OptIn(ExperimentalObjCName::class)
+@ObjCName(swiftName = "AnalyticsEvent")
 interface AnalyticsEvents
