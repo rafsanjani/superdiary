261c7270bd2d07ed442b6b40baba0993ca00fbc4
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-04-02 06:51:52 +0100

    Fix failing paparazzi tests The tests have now been moved into the android sourcesets

--- shared-ui/build.gradle.kts
+++ shared-ui/build.gradle.kts
@@ -1,7 +1,6 @@
 @file:Suppress("UnusedPrivateProperty")
 
 plugins {
-    alias(libs.plugins.paparazzi)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.ksp)
@@ -129,29 +128,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    sourceSets["main"].res.srcDirs("src/commonMain/resources")
-}
-
-plugins.withId("app.cash.paparazzi") {
-    afterEvaluate {
-        dependencies.constraints {
-            add("testImplementation", "com.google.guava:guava") {
-                attributes {
-                    attribute(
-                        TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
-                        objects.named(
-                            TargetJvmEnvironment::class.java,
-                            TargetJvmEnvironment.STANDARD_JVM,
-                        ),
-                    )
-                }
-                because(
-                    "LayoutLib and sdk-common depend on Guava's -jre published variant." +
-                        "See https://github.com/cashapp/paparazzi/issues/906.",
-                )
-            }
-        }
-    }
+    sourceSets["main"].res.srcDirs(
+        "src/commonMain/composeResources",
+    )
 }
 
 dependencies {
