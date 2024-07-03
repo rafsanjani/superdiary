9c27448735f7360be5c4e0702435faf07ec813c4
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-07-03 23:01:00 +0100

    Fix log messages not displaying for kermit

--- core/logging/src/commonMain/kotlin/com/foreverrafs/superdiary/core/logging/KermitLogger.kt
+++ core/logging/src/commonMain/kotlin/com/foreverrafs/superdiary/core/logging/KermitLogger.kt
@@ -2,19 +2,19 @@ package com.foreverrafs.superdiary.core.logging
 
 class KermitLogger : Logger {
     override fun v(tag: String, throwable: Throwable?, message: () -> String) {
-        co.touchlab.kermit.Logger.v(tag, throwable, message)
+        co.touchlab.kermit.Logger.v(tag = tag, throwable = throwable, messageString = message())
     }
 
     override fun d(tag: String, message: () -> String) {
-        co.touchlab.kermit.Logger.d(tag, message = message)
+        co.touchlab.kermit.Logger.d(tag = tag, messageString = message())
     }
 
     override fun e(tag: String, throwable: Throwable?, message: () -> String) {
-        co.touchlab.kermit.Logger.e(tag, throwable, message)
+        co.touchlab.kermit.Logger.e(tag = tag, throwable = throwable, messageString = message())
     }
 
     override fun i(tag: String, message: () -> String) {
-        super.i(tag, message)
+        co.touchlab.kermit.Logger.i(messageString = message(), tag = tag)
     }
 
     override fun w(tag: String, throwable: Throwable?, message: () -> String) {
