ccbd9de81e39b45e81208a4a3b73d09c9d08d704
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-10-06 20:34:41 +0100

    Use version catalog for mokkery

--- shared-data/build.gradle.kts
+++ shared-data/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("kotlin-parcelize")
     kotlin("multiplatform")
     id("com.superdiary.kover")
-    id("dev.mokkery") version "2.3.0"
+    alias(libs.plugins.mokkery)
 }
 
 sqldelight {
