262a53c5c3e046c01f0c538fec06f20e588007fa
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-11-06 19:20:16 +0000

    Add coverage badge

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import javax.xml.parsers.DocumentBuilderFactory
+
 plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
@@ -18,5 +20,38 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
+
+tasks.register("printLineCoverage") {
+    group = "verification"
+    doLast {
+        val report = file("build/reports/kover/report.xml")
+
+        val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
+        val rootNode = doc.firstChild
+        var childNode = rootNode.firstChild
+
+        var coveragePercent = 0.0
+
+        while (childNode != null) {
+            if (childNode.nodeName == "counter") {
+                val typeAttr = childNode.attributes.getNamedItem("type")
+                if (typeAttr.textContent == "LINE") {
+                    val missedAttr = childNode.attributes.getNamedItem("missed")
+                    val coveredAttr = childNode.attributes.getNamedItem("covered")
+
+                    val missed = missedAttr.textContent.toLong()
+                    val covered = coveredAttr.textContent.toLong()
+
+                    coveragePercent = (covered * 100.0) / (missed + covered)
+
+                    break
+                }
+            }
+            childNode = childNode.nextSibling
+        }
+
+        println("%.1f".format(coveragePercent))
     }
 }
