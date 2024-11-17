ae99c10e6c27e30ed34b1a43e4838d298eb755f7
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-11-17 08:03:39 +0000

    Show default avatar when not signed in

---
+++ shared-ui/src/androidUnitTest/kotlin/com/foreverrafs/superdiary/ui/TopAppBarSnapshotTests.kt
@@ -0,0 +1,53 @@
+package com.foreverrafs.superdiary.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import app.cash.paparazzi.Paparazzi
+import com.foreverrafs.superdiary.ui.components.SuperDiaryAppBar
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class TopAppBarSnapshotTests(
+    @TestParameter val snapshotDevice: SnapshotDevice,
+) {
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = snapshotDevice.config,
+        useDeviceResolution = true,
+    )
+
+    @Test
+    fun `Top App Bar`() {
+        paparazzi.snapshot {
+            SuperdiaryPreviewTheme {
+                Scaffold(
+                    topBar = {
+                        SuperDiaryAppBar()
+                    },
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = Color.Cyan)
+                            .padding(it),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text("hello World")
+                    }
+                }
+            }
+        }
+    }
+}
