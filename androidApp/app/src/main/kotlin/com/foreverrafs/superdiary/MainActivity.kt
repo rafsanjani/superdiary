c37193e884df56969c1b7b5b1026c3032fa2d4e4
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-12-16 00:34:29 +0000

    Print signing key in activity

--- androidApp/app/src/main/kotlin/com/foreverrafs/superdiary/MainActivity.kt
+++ androidApp/app/src/main/kotlin/com/foreverrafs/superdiary/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.foreverrafs.superdiary
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.testTagsAsResourceId
-import com.foreverrafs.superdiary.ui.App
+import java.security.MessageDigest
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContent {
-            enableEdgeToEdge()
-
-            App(
-                modifier = Modifier.semantics {
-                    testTagsAsResourceId = true
-                },
-            )
-        }
+        val key = getSigningKeySHA1(packageName, packageManager)
+        setContentView(
+            TextView(this).apply {
+                text = key
+            },
+        )
     }
+}
+
+fun getSigningKeySHA1(packageName: String, packageManager: PackageManager): String? = try {
+    val packageInfo: PackageInfo =
+        packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+
+    val cert =
+        packageInfo.signingInfo?.apkContentsSigners[0]?.toByteArray()
+
+    val messageDigest = MessageDigest.getInstance("SHA1")
+    if (cert != null) {
+        messageDigest.update(cert)
+    }
+    val sha1Bytes = messageDigest.digest()
+    sha1Bytes.joinToString(":") { String.format("%02X", it) }
+} catch (e: Exception) {
+    e.printStackTrace()
+    null
 }
