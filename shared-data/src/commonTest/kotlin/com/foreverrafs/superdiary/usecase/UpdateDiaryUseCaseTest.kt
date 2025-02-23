e628ba269fe22d6b8fd69ebbffad5e6bb3275696
Rafsanjani Aziz <coded_raf@yahoo.com>
2025-02-23 21:52:59 +0000

    Add more tests for use cases

--- shared-data/src/commonTest/kotlin/com/foreverrafs/superdiary/usecase/UpdateDiaryUseCaseTest.kt
+++ shared-data/src/commonTest/kotlin/com/foreverrafs/superdiary/usecase/UpdateDiaryUseCaseTest.kt
@@ -10,6 +10,7 @@ import com.foreverrafs.superdiary.data.datasource.LocalDataSource
 import com.foreverrafs.superdiary.database.Database
 import com.foreverrafs.superdiary.database.testSuperDiaryDatabase
 import com.foreverrafs.superdiary.domain.repository.DataSource
+import com.foreverrafs.superdiary.domain.usecase.GetAllDiariesUseCase
 import com.foreverrafs.superdiary.domain.usecase.UpdateDiaryUseCase
 import com.foreverrafs.superdiary.insertRandomDiaries
 import kotlin.test.AfterTest
