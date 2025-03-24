ebb684b69e54b78813e9cdea248b73767d195775
Rafsanjani Aziz <coded_raf@yahoo.com>
2025-03-24 05:54:48 +0000

    Update textfields to version with states

--- design-system/src/commonMain/kotlin/com/foreverrafs/superdiary/design/components/SuperDiaryInputField.kt
+++ design-system/src/commonMain/kotlin/com/foreverrafs/superdiary/design/components/SuperDiaryInputField.kt
@@ -89,6 +89,53 @@ fun SuperDiaryInputField(
 }
 
 @Composable
+fun SuperDiaryInputField(
+    label: String,
+    state: TextFieldState,
+    modifier: Modifier = Modifier,
+    onValueChange: (value: String) -> Unit = {},
+    errorLabel: String? = null,
+    placeholder: String? = null,
+    readOnly: Boolean = false,
+    isError: Boolean = false,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+) {
+    val text by snapshotFlow { state.text }.collectAsState(initial = state.text)
+    val currentOnTextChanged by rememberUpdatedState(onValueChange)
+
+    LaunchedEffect(text) {
+        currentOnTextChanged(text.toString())
+    }
+
+    TextField(
+        modifier = modifier
+            .fillMaxWidth(),
+        state = state,
+        isError = isError,
+        placeholder = {
+            if (placeholder != null) {
+                Text(
+                    text = placeholder,
+                )
+            }
+        },
+        keyboardOptions = keyboardOptions,
+        readOnly = readOnly,
+        supportingText = {
+            errorLabel?.let {
+                Text(it)
+            }
+        },
+        label = {
+            Text(
+                text = label,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        },
+    )
+}
+
+@Composable
 fun PasswordInputField(
     label: String,
     state: TextFieldState,
