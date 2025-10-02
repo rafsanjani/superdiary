7ea5c01c00ef4533300ffa4e1f8a5ea093d4cf9e
rafsanjani <9197459+rafsanjani@users.noreply.github.com>
2025-10-02 23:33:38 +0000

    Bump android version code to

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.foreverrafs.superdiary"
         minSdk = libs.versions.minimumSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 248
+        versionCode = 249
         versionName = "0.0.1"
 
         val sentryBaseUrl = System.getenv("SENTRY_BASE_URL_ANDROID") ?: ""
