08fc451e676f9569d3be9d4294150c35f2c5c23d
Rafsanjani Aziz <coded_raf@yahoo.com>
2025-11-03 23:27:58 +0000

    Skip running ios tests on CI These tests are too slow, it's a risk we're willing to take for now

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 @file:Suppress("UnstableApiUsage")
 
+
 pluginManagement {
     repositories {
         mavenCentral()
@@ -22,9 +23,11 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             from("io.github.rafsanjani:versions:2025.10.26")
+            version("gradle", "8.12.0")
             // Because 3.2.4 is making internal calls to android.util.Log which dey borst my mind
             version("supabase","3.2.3")
             version("compose-multiplatform", "1.10.0-alpha03")
+            version("paparazzi", "2.0.0-alpha02")
         }
     }
 }
