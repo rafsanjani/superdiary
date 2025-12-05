7690933e2a398d5f7bce9b596762f22df573a0d4
Rafsanjani Aziz <coded_raf@yahoo.com>
2025-12-05 22:20:31 +0000

    Fix navigation crash on iOS

--- navigation/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/navigation/NavigationState.kt
+++ navigation/src/commonMain/kotlin/com/foreverrafs/superdiary/ui/navigation/NavigationState.kt
@@ -19,9 +19,6 @@ package com.foreverrafs.superdiary.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSerializable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
@@ -29,38 +26,20 @@ import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.rememberDecoratedNavEntries
-import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.savedstate.serialization.SavedStateConfiguration
 
 /**
- * Create a navigation state that persists config changes and process death.
+ * Create a navigation state that persists config changes and process death on Android.
+ * The concept of process death is non-existent on non-android operating systems so it just creates
+ * and remember a navigation state on non-android platforms
  */
 @Composable
-fun rememberNavigationState(
+expect fun rememberNavigationState(
     startRoute: NavKey,
     topLevelRoutes: Set<NavKey>,
     configuration: SavedStateConfiguration,
-): NavigationState {
-    val topLevelRoute = rememberSerializable(
-        startRoute,
-        topLevelRoutes,
-        configuration = configuration,
-    ) {
-        mutableStateOf(startRoute)
-    }
-
-    val backStacks =
-        topLevelRoutes.associateWith { key -> rememberNavBackStack(configuration, key) }
-
-    return remember(startRoute, topLevelRoutes) {
-        NavigationState(
-            startRoute = startRoute,
-            topLevelRoute = topLevelRoute,
-            backStacks = backStacks,
-        )
-    }
-}
+): NavigationState
 
 /**
  * State holder for navigation state.
