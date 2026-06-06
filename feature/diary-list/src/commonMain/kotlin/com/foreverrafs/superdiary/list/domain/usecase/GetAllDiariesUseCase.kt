a6421d7f40591a05fe76c4b39440632aeab14ea4
Rafsanjani Aziz <coded_raf@yahoo.com>
2026-06-06 11:13:12 +0100

    Fix lint violations

--- feature/diary-list/src/commonMain/kotlin/com/foreverrafs/superdiary/list/domain/usecase/GetAllDiariesUseCase.kt
+++ feature/diary-list/src/commonMain/kotlin/com/foreverrafs/superdiary/list/domain/usecase/GetAllDiariesUseCase.kt
@@ -1,11 +1,9 @@
 package com.foreverrafs.superdiary.list.domain.usecase
 
 import androidx.paging.PagingData
-import com.foreverrafs.superdiary.data.Result
 import com.foreverrafs.superdiary.domain.model.Diary
 import com.foreverrafs.superdiary.list.domain.repository.DiaryListRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 internal class GetAllDiariesUseCase(
     private val repository: DiaryListRepository,
